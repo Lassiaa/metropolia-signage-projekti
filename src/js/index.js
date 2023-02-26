dd203347d1776a9f30485ed16056fe334c7c674d
ofisch <onni.fischer@gmail.com>
2023-02-26 13:29:54 +0200

    tiedotteiden haku json-tiedostosta

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,31 @@
 'use strict';
 
-const info = ['moi täs tiedote',
-  'toinen tiedote',
-  'testi',
-  'toimiiks tää',
-];
+// polku info.json-tiedostoon: '../src/assets/info.json'
 
-const render = (target) => {
-  const infoList = document.querySelector('#info-list');
-  for (const i of target) {
-    console.log(i);
-    const li = document.createElement('li');
-    li.textContent = i;
-    infoList.append(li);
+async function getInfo() {
+  let url = '../src/assets/info.json';
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
   }
-};
+}
 
-render(info);
+async function renderInfo() {
+  let info = await getInfo();
+  let html = '';
+
+  info.forEach(node => {
+    let htmlSegment = `<li>
+      ${node.title}
+      </li>`;
+
+    html += htmlSegment;
+  });
+
+  let container = document.querySelector('#info-list');
+  container.innerHTML = html;
+}
+
+renderInfo();
