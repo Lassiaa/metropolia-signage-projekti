b50749bd0ca7fd02ffa4d18e53cd18aa0f67ed83
Lassiaa <lassiaa@metropolia.fi>
2023-02-27 17:30:56 +0200

    Kaikki toiminnallisuus yhdistetty välikatselmusta varten

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,54 @@
 'use strict';
 
+let coursesEn = [];
+let coursesFi = [];
+let currentDate = new Date().toISOString().slice(0, 10)
+/**
+fetch(`https://www.sodexo.fi/ruokalistat/output/daily_json/158/${currentDate}`)
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data.courses[1].title_fi);
+    let foodContainer = document.querySelector('#food');
+    let text;
+   for (let i = 1;i < 5;i++){
+        coursesFi.push(data.courses[i].title_fi);
+        coursesEn.pusj(data.courses[i].title_en)
+   }
+  })
+  .catch(error => console.log(error));
+*/
+
+const getRuokaLista = async () => {
+  try {
+    const url = `https://www.sodexo.fi/ruokalistat/output/daily_json/158/${currentDate}`;
+    const res = await fetch(url);
+    console.log(res.ok);
+    const data = await res.json();
+    return (data);
+  } catch (err) {
+    console.error(err)
+  }
+};
+
+getRuokaLista().then((data) => {
+
+  let courses = data.courses;
+  let size = Object.keys(courses).length;
+  let html = "";
+  let foodContainer = document.querySelector('#food');
+  for (let i = 1; i <= size; i++) {
+    html += `<li>${courses[i].title_fi}</li>`
+  }
+  foodContainer.innerHTML += html;
+});
+
+const tulosta = (menu, order = 'asc') => {
+  console.log(coursesEn);
+  console.log(coursesFi);
+};
+
 // polku info.json-tiedostoon: '../src/assets/info.json'
 /**
  * 
@@ -36,3 +85,4 @@ async function renderInfo() {
 }
 
 renderInfo();
+
---
+++ src/js/ruokalistadata.js
@@ -0,0 +1,17 @@
+const coursesEn = [];
+const coursesFi = [];
+
+fetch('https://www.sodexo.fi/ruokalistat/output/daily_json/152/2023-01-30')
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => console.log(error));
+
+const tulosta = (menu, order='asc') => {
+    console.log(coursesEn);
+    console.log(coursesFi);
+};
+console.log("123");
