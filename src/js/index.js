9578c4eb7df4a45452737fa22188ccf146c000bb
ofisch <onni.fischer@gmail.com>
2023-03-02 21:44:56 +0200

    info-moduuli toimii

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,5 @@
 'use strict';
 
-// polku info.json-tiedostoon: '../src/assets/info.json'
-/**
- * 
- * @returns json-tiedoston sisällön
- */
-async function getInfo() {
-  //let url = '../src/assets/info.json';
-  let url = '../src/assets/info.json';
-  try {
-    let res = await fetch(url);
-    return await res.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
+const info = require('./info');
 
-/**
- * tulostaa json-tiedoston sisällön "tiedotteet"-osioon
- */
-async function renderInfo() {
-  let info = await getInfo();
-  let html = '';
-
-  info.forEach(node => {
-    let htmlSegment = `<li>
-      ${node.title}
-      </li>`;
-
-    html += htmlSegment;
-  });
-
-  let container = document.querySelector('#info-list');
-  container.innerHTML = html;
-}
-
-renderInfo();
+info.renderInfo();
