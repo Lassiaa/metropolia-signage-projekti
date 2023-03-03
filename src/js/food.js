22260d6f63f7f60dce84b6edd0550cd6dee915d8
eetupas <79096066+eetupas@users.noreply.github.com>
2023-03-03 17:24:25 +0200

    Karamalmin ruokalista toimii

--- src/js/food.js
+++ src/js/food.js
@@ -4,12 +4,11 @@ let currentDate = new Date().toISOString().slice(0, 10)
 
 const getFood = async () => {
     try {
-        const url = `https://www.compass-group.fi/menuapi/week-menus?costCenter=3208&date=${currentDate}T16%3A08%3A11.953Z&language=fi`;
+        const url = `https://www.compass-group.fi/menuapi/feed/json?costNumber=3208&language=fi`;
         const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(url)}`;
         const res = await fetch(proxyUrl);
         console.log(res.ok);
         const data = await res.json();
-        console.log(data)
         return (data);
     } catch (err) {
         console.error(err)
@@ -22,15 +21,23 @@ const renderFood = async () => {
     let day = date.getDate();
     let month = date.getMonth();
     let monthText = date.toLocaleDateString('default', { month: 'long'});
+    
+    const obj = await getFood();
+    const obj2 = JSON.parse(obj.contents);
+    const ruokalista = obj2.MenusForDays[0].SetMenus;
+    let text = '';
+    for(let i = 0; i < ruokalista.length; i++){
+        for (let j = 0; j < ruokalista[i].Components.length; j++) {
+        text += `<li>${ruokalista[i].Components[j]}</li><br>`;
+        }
+    }
 
-    let food = await getFood();
-    // food.contents on PIHVI
-    console.log(food.contents);
+    let food = document.querySelector('#food');
+    food.innerHTML += text;
 
-
+    
+    
     // TÄHÄN ruokalistan tulostaminen ruokalista-osioon
-
-
     // ruoka-osion otsikko
     let title = document.querySelector('#food-title');
     title.innerHTML = `ruokalista tänään ${day}. ${monthText}ta`;
