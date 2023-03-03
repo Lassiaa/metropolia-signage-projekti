d9366a1039a488978aae989eb41f0886361ed417
ofisch <onni.fischer@gmail.com>
2023-03-03 18:07:45 +0200

    koodia siivottu

--- src/js/food.js
+++ src/js/food.js
@@ -1,7 +1,9 @@
 'use strict';
 
-let currentDate = new Date().toISOString().slice(0, 10)
-
+/**
+ * 
+ * @returns ruokalista-apin jsonin
+ */
 const getFood = async () => {
     try {
         const url = `https://www.compass-group.fi/menuapi/feed/json?costNumber=3208&language=fi`;
@@ -15,32 +17,30 @@ const getFood = async () => {
     }
 };
 
+/**
+ * tulostaa ruokalistan "ruokalista"-osioon
+ */
 const renderFood = async () => {
-    // päivämääräjutut
-    const date = new Date();
-    let day = date.getDate();
-    let month = date.getMonth();
-    let monthText = date.toLocaleDateString('default', { month: 'long'});
-    
-    const obj = await getFood();
-    const obj2 = JSON.parse(obj.contents);
-    const ruokalista = obj2.MenusForDays[0].SetMenus;
+    const food = await getFood();
+    const foodContent = JSON.parse(food.contents);
+    const menu = foodContent.MenusForDays[0].SetMenus;
     let text = '';
-    for(let i = 0; i < ruokalista.length; i++){
-        for (let j = 0; j < ruokalista[i].Components.length; j++) {
-        text += `<li>${ruokalista[i].Components[j]}</li><br>`;
+
+    for (let i in menu) {
+        for (let j in menu[i].Components) {
+            text += `<li>${menu[i].Components[j]}</li><br>`;
         }
     }
 
-    let food = document.querySelector('#food');
-    food.innerHTML += text;
+    let container = document.querySelector('#food-list');
+    container.innerHTML += text;
 
-    
-    
-    // TÄHÄN ruokalistan tulostaminen ruokalista-osioon
-    // ruoka-osion otsikko
+    const date = new Date();
+    let day = date.getDate();
+    let monthText = date.toLocaleDateString('default', { month: 'long' });
+
     let title = document.querySelector('#food-title');
     title.innerHTML = `ruokalista tänään ${day}. ${monthText}ta`;
 };
 
-export {renderFood};
+export { renderFood };
