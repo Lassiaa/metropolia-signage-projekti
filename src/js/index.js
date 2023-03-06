91a0b64534622a93cda7cbbc7c4d7a65516e1d8f
Lassiaa <lassiaa@metropolia.fi>
2023-03-06 19:04:23 +0200

    Temp

--- src/js/index.js
+++ src/js/index.js
@@ -9,11 +9,18 @@ renderInfo();
 renderFood();
 renderHslData();
 
+// Get new menu data every 50s
 const foodRefresh = window.setInterval(() => {
     renderFood();
 }, 50000);
 
-
+// Get new info data every 50s
 const infoRefresh = window.setInterval(() => {
     renderInfo();
 }, 50000);
+
+
+// Get new HSL data every 30s
+const hslRefresh = window.setInterval(() => {
+    renderHslData();
+}, 30000);
