0e96867b2d6b95cd50196569460f9ba396f35b3c
Lassiaa <lassiaa@metropolia.fi>
2023-03-06 18:34:39 +0200

    Bussi aikataulu ei näytä alle 4min aikatauluja

--- src/js/hsl.js
+++ src/js/hsl.js
@@ -1,6 +1,12 @@
 'use strict';
 
+/**
+ * Retrieves  data from HSL API with GraphQL query
+ * Converts it into json data
+ * @returns json data from HSL API
+ */
 const getHslData = async () => {
+    // GraphQL query
     const query = `
         query {
           stops(name: "Karanristi Karamalmens") {
@@ -25,6 +31,7 @@ const getHslData = async () => {
         }
     `;
 
+    // Make the connection to HSL API and get data as json
     try {
         const apiUrl = "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql";
         const res = await fetch(apiUrl, {
@@ -44,44 +51,60 @@ const getHslData = async () => {
     }
 };
 
+/**
+ * Get the bus schedules for every stop from the json data
+ * Send the data to array so all stops are in one place
+ * Sort the array by busses arrival time
+ * Send 6 first bus schedules to index.html
+ */
 const renderHslData = async () => {
     const hslData = await getHslData();
-    console.log(hslData);
-
+    console.log(hslData)
     const hslArray = [];
+    const enoughTimeArray = [];
     let text = '';
 
+    // Clear old data from page
     let container = document.querySelector('#hsl-data');
     container.innerHTML = text;
 
+    //Go thru stops
     for (let stop = 0; stop < hslData.data.stops.length; stop++) {
         const busStopName = JSON.stringify(hslData.data.stops[stop].name).replace(/"/g, '');
         const busStopCode = JSON.stringify(hslData.data.stops[stop].code).replace(/"/g, '');
 
+        //Go thru busses within stops
         for (let bus = 0; bus < hslData.data.stops[stop].stoptimesWithoutPatterns.length; bus++) {
-          const busName = JSON.stringify(hslData.data.stops[stop].stoptimesWithoutPatterns[bus].headsign).replace(/"/g, '');
-          const busNum = JSON.stringify(hslData.data.stops[stop].stoptimesWithoutPatterns[bus].trip.route.shortName).replace(/"/g, '');
-          const busArrival = JSON.stringify(hslData.data.stops[stop].stoptimesWithoutPatterns[bus].realtimeArrival);
+            const busName = JSON.stringify(hslData.data.stops[stop].stoptimesWithoutPatterns[bus].headsign).replace(/"/g, '');
+            const busNum = JSON.stringify(hslData.data.stops[stop].stoptimesWithoutPatterns[bus].trip.route.shortName).replace(/"/g, '');
+            const busArrival = JSON.stringify(hslData.data.stops[stop].stoptimesWithoutPatterns[bus].realtimeArrival);
 
-          // Converting seconds to utc time and converting to desired format
-          const dateUTC  = new Date(null);
-          dateUTC .setUTCSeconds(busArrival); 
-          const [dayOfTheWeek, date, month, year, hour, min, sec] = dateUTC.toUTCString().split(/:| /);
-          const desiredTimeFormat = `${hour}:${min}`;
+            // Get current time in seconds since midnight
+            const now = new Date();
+            const midnight = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+            const seconds = Math.floor((now - midnight) / 1000);    
 
-          hslArray.push('klo.' + desiredTimeFormat + ', ' + busStopName + ', ' + busStopCode + ', ' + busNum + ', ' + busName)
+            // Arrivaltime is in seconds so convert it to utc time and desired format
+            const dateUTC  = new Date(null);
+            dateUTC .setUTCSeconds(busArrival); 
+            const [dayOfTheWeek, date, month, year, hour, min, sec] = dateUTC.toUTCString().split(/:| /);
+            const desiredTimeFormat = `${hour}:${min}`;
 
+            hslArray.push(desiredTimeFormat + `<br>` + '<strong>Bussi: </strong>' + busNum + ', ' + busName + ', ' + busStopName + `<br>` + '<strong>Pysäkki: </strong>' + busStopCode + ', ' + busStopName);
+
+            // Don't show busses that are coming in under 4 minutes so user has time to walk to the stop
+            console.log(Math.abs(seconds - busArrival));
+            if (Math.abs(seconds - busArrival) > 240) {
+              enoughTimeArray.push(desiredTimeFormat + `<br>` + '<strong>Bussi: </strong>' + busNum + ', ' + busName + ', ' + busStopName + `<br>` + '<strong>Pysäkki: </strong>' + busStopCode + ', ' + busStopName);
+            }
         }
     }
-
     hslArray.sort();
+    enoughTimeArray.sort();
 
-    for (let i = 0; i < hslArray.length && i < 5; i++) {
-      text += `<li>` + hslArray[i] + `</li><br>`;
+    for (let i = 0; i < enoughTimeArray.length && i < 6; i++) {
+        text += `<li>` + '<strong>klo.</strong>' + enoughTimeArray[i] + `</li><br>`;
     }
-
-    console.log(hslArray);
-
     container.innerHTML += text;
 };
 
