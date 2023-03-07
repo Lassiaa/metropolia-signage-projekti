e98c7922758e3e4180f854b84d216bab80714ba9
eetupas <79096066+eetupas@users.noreply.github.com>
2023-03-07 13:58:37 +0200

    Jsonit tulostuu

--- src/js/index.js
+++ src/js/index.js
@@ -10,19 +10,47 @@ renderInfo();
 renderFood();
 renderHslData();
 renderWeather();
+//Varmistetaan että kaikki api fetchit menee läpi ja tulostuu sivulle
+let s = false;
+let counter = 0;
+let container = document.querySelector('#hsl-data');
+let fcontainer = document.querySelector('#food-list');
+let wcontainer = document.querySelector('#weather-temp');
+let icontainer = document.querySelector('#info-carousel');
+while(s !== true){
+    counter++;
+    if (container.innerHTML == ""){
+        await renderHslData();
+    }
+    if (fcontainer.innerHTML == ""){
+        await renderFood();
+    }
+    if (wcontainer.innerHTML == ""){
+        await renderWeather();
+    }
+    if (icontainer.innerHTML == ""){
+        await renderInfo();
+    }
+    if (container.innerHTML !== "" && fcontainer.innerHTML !== "" && wcontainer.innerHTML !== "" && icontainer.innerHTML !== ""){
+        s = true;
+    }
+    if (counter == 100){
+        s = true;
+    }
+}
 
-// Get new menu and weather data every 50s
+// Get new menu and weather data every 1h
 const dataRefresh = window.setInterval(() => {
     renderFood();
     renderWeather();
-}, 50000);
+}, 6000000);
 
-// Get new info data every 50s
+// Get new info data every 1h
 const infoRefresh = window.setInterval(() => {
     renderInfo();
-}, 50000);
+}, 6000000);
 
-// Get new HSL data every 5s
+// Get new HSL data every 30s
 const hslRefresh = window.setInterval(() => {
     renderHslData();
-}, 5000);
+}, 30000);
